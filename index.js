1fcec2a80c4789964264079b412a52a27e2ab9e2
Florencia <florencianoelcarrillo@gmail.com>
2022-10-08 18:28:11 -0300

    se cambia la forma de calculo usando eval y se agrega la funcionalidad a los parentesis

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 let botonesNumero = document.getElementsByClassName("botonNumero");
 let pantalla = document.getElementsByClassName("pantalla")[0];
 let botonSuma = document.getElementById("botonSuma");
-let botonesOperando = document.getElementsByClassName("botonOperando")
-let operando = "";
-let primerFactor = 0;
-let segundoFactor = 0;
+let operacion = 0;
 let resultado = "";
 let botonBorrar = document.getElementById("botonBorrar");
 
@@ -16,46 +13,16 @@ for (i = 0; i < botonesNumero.length; i++) {
     }
 }
 
-for (let i = 0; i < botonesOperando.length; i++) {
-    const boton = botonesOperando[i];
-    boton.onclick = () => {
-        primerFactor = parseFloat(pantalla.textContent);
-        pantalla.innerHTML = "";
-        operando = boton.textContent;
-    }
-
-}
 
 botonBorrar.onclick = () => {
     pantalla.innerHTML = "";
-    primerFactor = 0;
-    segundoFactor = 0;
-    operando = "";
+    operacion = 0;
 }
 
 botonIgual.onclick = () => {
-    segundoFactor = parseInt(pantalla.textContent);
-    switch (operando) {
-        case "+":
-            resultado = primerFactor + segundoFactor;
-            pantalla.innerHTML = resultado;
-            break;
-        case "-":
-            resultado = primerFactor - segundoFactor;
-            pantalla.innerHTML = resultado;
-            break;
-        case "*":
-            resultado = primerFactor * segundoFactor;
-            pantalla.innerHTML = resultado;
-            break;
-        case "/":
-            resultado = primerFactor / segundoFactor;
-            pantalla.innerHTML = resultado;
-            break;
-
-        default:
-            break;
-    }
+    operacion = pantalla.textContent;
+    resultado = eval(operacion); //eval recibe strings
+    pantalla.innerHTML = resultado;
 }
 
 let numeroPantalla = "";
